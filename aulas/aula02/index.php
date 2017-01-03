b2894e8e40e51a44086e5b656e9715fa0ce05ca4
Eric Ferraz <universidadeprogramacao@gmail.com>
2017-01-02 23:56:20 -0200

    aula 5

--- aulas/aula02/index.php
+++ aulas/aula02/index.php
@@ -24,53 +24,57 @@
 
         <script src="../../js/angular-1.5.0/angular.js"></script>
         <script src="../../js/angular-1.5.0/angular-messages.js"></script>
+        <script src="../../js/angular-1.5.0/i18n/angular-locale_pt-br.js"></script>
 
         <script>
             angular.module("listaTelefonica", ["ngMessages"]);
             //scope faz a mediação entre view e controller
             angular.module('listaTelefonica').controller('listaTelefonicaController', function ($scope) {
-                $scope.app = "Lista Telefônica";
-                $scope.contatos = [
-                    {nome: "Eric", telefone: "(14) 98872-3232", cor: 'red'},
-                    {nome: "Ana Maria", telefone: "(43) 88872-3202", cor: 'yellow'},
-                    {nome: "Daniele", telefone: "(14) 98982-7868", cor: 'blue'},
-                    {nome: "Vania Soares", telefone: "(14) 8198-3242", cor: 'purple'}
-                ];
-                $scope.operadoras = [
-                    {nome: "OI", categoria: "CEL"},
-                    {nome: "TIM", categoria: "CEL"},
-                    {nome: "CLARO", categoria: "CEL"},
-                    {nome: "VIVO", categoria: "CEL,FIXO"}
-                ];
-                //cria uma função chamada adicionarContato visível para esse escopo
-                $scope.adicionarContato = function (contato) {
-                    //adiciona um novo objeto ao fim do array
+            $scope.app = "Lista Telefônica";
+            $scope.contatos = [
+            {data: new Date(), nome: "Eric", telefone: "(14) 98872-3232", cor: 'red', operadora: {nome: 'TIM', categoria: 'CEL'}},
+            {data: new Date(), nome: "Ana Maria", telefone: "(43) 88872-3202", cor: 'yellow', operadora: {nome: 'VIVO', categoria: 'CEL'}},
+            {data: new Date(), nome: "Daniele", telefone: "(14) 98982-7868", cor: 'blue', operadora: {nome: 'OI', categoria: 'CEL'}},
+            {data: new Date(), nome: "Vania Soares", telefone: "(14) 8198-3242", cor: 'purple', operadora: {nome: 'CLARO', categoria: 'CEL'}}
+            ];
+            $scope.operadoras = [
+            {nome: "OI", categoria: "CEL", preco: 2},
+            {nome: "TIM", categoria: "CEL", preco: 1},
+            {nome: "CLARO", categoria: "CEL", preco: 3},
+            {nome: "VIVO", categoria: "CEL,FIXO", preco: 5}
+            ];
+            //cria uma função chamada adicionarContato visível para esse escopo
+            $scope.adicionarContato = function (contato) {
+            //adiciona um novo objeto ao fim do array
 
-                    //1- a forma ruim  de se fazer
-                    // $scope.contatos.push({nome:$scope.nome,telefone:$scope.telefone});
+            //1- a forma ruim  de se fazer
+            // $scope.contatos.push({nome:$scope.nome,telefone:$scope.telefone});
 
-                    //2- a forma rasuável  de se fazer
-                    //$scope.contatos.push({nome: nome, telefone: telefone});
+            //2- a forma rasuável  de se fazer
+            //$scope.contatos.push({nome: nome, telefone: telefone});
 
-                    //3- a melhor formar
-                    $scope.contatos.push(angular.copy(contato));
+            //3- a melhor formar
+            $scope.contatos.push(angular.copy(contato));
+            //seta o forma para a propriedade original, como se nunca tivesse sido selecionado
+            $scope.contatoForm.$setPristine();
+            };
+            $scope.apagarContato = function (contatos) {
 
-                    //seta o forma para a propriedade original, como se nunca tivesse sido selecionado
-                    $scope.contatoForm.$setPristine();
-                };
-                $scope.apagarContato = function (contatos) {
-
-                    $scope.contatos = contatos.filter(function (contato) {
-                        if (!contato.selecionado) {
-                            return contato;
-                        }
-                    });
-                };
-                $scope.isContatosSelecionados = function (contatos) {
-                    return contatos.some(function (contato) {
-                        return contato.selecionado;
-                    });
-                };
+            $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) {
+            return contato;
+            }
+            });
+            };
+            $scope.isContatosSelecionados = function (contatos) {
+            return contatos.some(function (contato) {
+            return contato.selecionado;
+            });
+            };
+            $scope.ordernarPor = function (campo) {
+            $scope.criterioDeOrdenacao = campo;
+            $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
+            };
             });
         </script>
     </head>
@@ -83,6 +87,8 @@
                 </div>
             </div>
 
+            {{100| number:2}}
+
             <div class="panel-body">
 
                 <form name="contatoForm" class="form-horizontal">
@@ -91,7 +97,7 @@
                         <div class="col-sm-6">
                             <input type="text" name="nome" ng-model="contato.nome" ng-required="true" ng-minlength="3" class="form-control">
                         </div>
-                        {{contato.nome}}
+                        {{contato.nome| uppercase}}
                     </div>
                     <div class="form-group">
 
@@ -110,7 +116,7 @@
 
                         <label class="col-sm-2 control-label">Operadora:</label>
                         <div class="col-sm-4">
-                            <select name="operadora" class="form-control" ng-model="contato.operadora"  ng-required="true" ng-options="operadora.nome for operadora in operadoras">
+                            <select name="operadora" class="form-control" ng-model="contato.operadora"  ng-required="true" ng-options="operadora.nome + '( ' + (operadora.preco | currency) + ')' for operadora in operadoras | orderBy:'nome'">
                                 <option value="">Selecione uma operadora</option>
                             </select>
                         </div>
@@ -180,12 +186,19 @@
                 </form>
                 <hr/>
 
+                <input type="text" class="form-control" placeholder="Digite um termo para pesquisar" ng-model="criterioDePesquisa">
+
                 <table class="table table-bordered" ng-show="contatos.length > 0">
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Nome</th>
-                            <th>Telefone</th>
+                            <th>
+                                <a class="focus" href="#" ng-click="ordernarPor('nome')">Nome</a></th>
+                            <th>Data de cadastro</th>
+                            <th>
+                                <a class="focus" href="#" ng-click="ordernarPor('telefone')">Telefone</a></th>
+
+                            </th>
                             <th>Operadora</th>
                             <th style="width: 50px;"></th>
                         </tr>
@@ -194,14 +207,15 @@
                     <tbody>
                         <tr ng-class="{
                             'selecionado negrito'
-                                    : contato.selecionado}" ng-repeat="contato in contatos">
+                                    : contato.selecionado}" ng-repeat="contato in contatos | limitTo:2 | filter:criterioDePesquisa | orderBy:criterioDeOrdenacao:direcaoOrdenacao">
                             <td>
                                 <input type="checkbox"
                                        ng-model="contato.selecionado">
                             </td>
-                            <td>{{contato.nome}}</td> 
+                            <td>{{contato.nome| uppercase | limitTo:6}}</td> 
+                            <td>{{contato.data| date:'dd/MM/yyyy HH:mm:ss'}}</td> 
                             <td>{{contato.telefone}}</td>
-                            <td>{{contato.operadora.nome}}</td>
+                            <td>{{contato.operadora.nome| lowercase}}</td>
                             <td>
                                 <div style="width: 50px;height: 50px;border-radius: 50%;" ng-style="{
                                     'background-color'
